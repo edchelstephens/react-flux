243ff8a923c59548a84bad0245a14ed0ea407069
Edchel Stephen <edchelstephens@gmail.com>
2021-08-30 18:33:47 +0800

    use header to create app header.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import AboutPage from "./AboutPage";
 import HomePage from "./HomePage";
+import Header from "./common/Header";
 
 function App() {
-  const route = window.location.pathname;
+  function getPage() {
+    const route = window.location.pathname;
+    let component = <HomePage />;
+    if (route === "/about") component = <AboutPage />;
 
-  let component = <HomePage />;
-  if (route === "/about") component = <AboutPage />;
-
-  return component;
+    return component;
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
