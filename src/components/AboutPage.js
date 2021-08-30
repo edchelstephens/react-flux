a6b861a772784ad0fe6a94046510a4838432214f
Edchel Stephen <edchelstephens@gmail.com>
2021-08-30 18:17:17 +0800

    use React.Fragment for wrapper.

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>About</h2>
+        <p>About page with class component.</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
