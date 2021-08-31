b4a6b5cdcad97f3e9060d5bbef5b12e65c4cb83a
Edchel Stephen <edchelstephens@gmail.com>
2021-08-31 08:14:03 +0800

    constructor syntax.

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { getCourses } from "../api/courseApi";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: [],
+    };
+  }
   render() {
     return <h2>Courses</h2>;
   }
