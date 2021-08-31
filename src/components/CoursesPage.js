d982aef00a0b0e6df95044625e268f589fc1f3ac
Edchel Stephen <edchelstephens@gmail.com>
2021-08-31 08:19:27 +0800

    componentDidMount() set courses.

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -2,11 +2,12 @@ import React from "react";
 import { getCourses } from "../api/courseApi";
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      courses: [],
-    };
+  state = {
+    courses: [],
+  };
+
+  componentDidMount() {
+    getCourses().then((courses) => this.setState({ courses: courses }));
   }
   render() {
     return <h2>Courses</h2>;
