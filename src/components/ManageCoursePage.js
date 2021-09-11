92b657200e1c6f694b19b14ea84010a636d57553
Edchel Stephen <edchelstephens@gmail.com>
2021-09-11 22:35:13 +0800

    native Prompt component for react which is alert in javascript.

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 
 const ManageCoursePage = (props) => {
-  debugger;
   return (
     <>
       <h2> Manage Course</h2>
+      <Prompt when={true} message="Sure you want to leave?" />
     </>
   );
 };
