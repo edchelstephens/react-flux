8f78143e1372a5d7f9105c3964a69aab482b8646
Edchel Stephen <edchelstephens@gmail.com>
2021-09-21 21:02:11 +0800

    CourseForm component.

---
+++ src/components/CourseForm.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const CourseForm = (props) => {
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <div className="field">
+          <input
+            id="title"
+            type="text"
+            name="title"
+            className="form-control"
+            value=""
+          ></input>
+        </div>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="author">Author</label>
+        <div className="field">
+          <select
+            id="author"
+            name="authorId"
+            onChange={props.onChange}
+            value=""
+            className="form-control"
+          >
+            <option value=""></option>
+            <option value="1">Edchel Stephen</option>
+            <option value="2">Robert Martin</option>
+            <option value="3">Cory House</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="category">Category</label>
+        <div className="field">
+          <input
+            type="text"
+            id="category"
+            name="category"
+            className="form-control"
+            value=""
+          ></input>
+        </div>
+      </div>
+
+      <input type="submit" value="Save" className="btn btn-primary"></input>
+    </form>
+  );
+};
+
+export default CourseForm;
