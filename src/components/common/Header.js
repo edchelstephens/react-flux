7703f1a92b0c1ce9a6febabc252353e50911e63b
Edchel Stephen <edchelstephens@gmail.com>
2021-09-21 21:12:31 +0800

    add nav styling.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,18 +4,30 @@ import { NavLink } from "react-router-dom";
 function Header() {
   const activeStyle = { color: "orange" };
   return (
-    <nav>
-      <NavLink activeStyle={activeStyle} exact to="/">
-        Home
-      </NavLink>
-      {" | "}
-      <NavLink activeStyle={activeStyle} exact to="/courses">
-        Courses
-      </NavLink>
-      {" | "}
-      <NavLink activeStyle={activeStyle} exact to="/about">
-        About
-      </NavLink>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="navbar-nav">
+        <NavLink className="nav-link" activeStyle={activeStyle} exact to="/">
+          Home
+        </NavLink>
+        {" | "}
+        <NavLink
+          className="nav-link"
+          activeStyle={activeStyle}
+          exact
+          to="/courses"
+        >
+          Courses
+        </NavLink>
+        {" | "}
+        <NavLink
+          className="nav-link"
+          activeStyle={activeStyle}
+          exact
+          to="/about"
+        >
+          About
+        </NavLink>
+      </div>
     </nav>
   );
 }
