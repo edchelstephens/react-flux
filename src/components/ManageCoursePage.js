c342c4146714b1486fbee3fe96d518e59dfe8f8f
Edchel Stephen <edchelstephens@gmail.com>
2021-09-24 21:26:33 +0800

    add course form validation, if not valid then return early, do nothing, do not call the api and just populate the errors object for displaying of errors.

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -5,6 +5,7 @@ import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
 
 const ManageCoursePage = (props) => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -20,8 +21,31 @@ const ManageCoursePage = (props) => {
     });
   }
 
+  function isFormValid() {
+    const form_errors = {};
+
+    if (!course.title) {
+      form_errors.title = "Title is required";
+    }
+    if (!course.authorId) {
+      form_errors.authorId = "Author is required";
+    }
+    if (!course.category) {
+      form_errors.category = "Category is required";
+    }
+
+    setErrors(form_errors);
+
+    let no_errors = Object.keys(form_errors).length === 0;
+
+    return no_errors;
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    if (!isFormValid()) {
+      return;
+    }
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Success! Course saved!");
@@ -35,6 +59,7 @@ const ManageCoursePage = (props) => {
         course={course}
         onChange={handleChage}
         onSubmit={handleSubmit}
+        errors={errors}
       />
     </>
   );
